d11e6f69877e2c34d6d349a39f23726e90dbb022
takamuu <mr.takamuu@gmail.com>
2021-10-19 14:28:24 +0900

    rendering_comfirmation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
-// import "./styles.css";
+import React, { useState, useCallback, useMemo } from "react";
+import "./styles.css";
+import { ChildArea } from "./ChildArea";
 
 export const App = () => {
   console.log("App");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
 
   return (
     <div className="App">
-      <input />
+      <input value={text} onChange={onChangeText} />
       <br />
       <br />
-      <button>表示</button>
-      <>
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose}/>
     </div>
   );
 };
