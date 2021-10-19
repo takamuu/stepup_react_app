bd6b8cbdb4a6e3a237dcc9013eb2b7b87a3c0e64
takamuu <mr.takamuu@gmail.com>
2021-10-19 17:26:19 +0900

    Nested page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 
 export default function App() {
@@ -13,10 +15,31 @@ export default function App() {
         <Link to="/page1">Page1</Link>
         <br />
         <Link to="/page2">Page2</Link>
-        <Home />
-        <Page1 />
-        <Page2 />
       </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )} 
+        />
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 };
